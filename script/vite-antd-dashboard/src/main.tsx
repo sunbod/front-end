192039c57d81b57b961cbbad18a6bb09de6edc5f
bo <1533265268@qq.com>
2024-04-07 17:56:38 +0800

    init project

--- script/vite-antd-dashboard/src/main.tsx
+++ script/vite-antd-dashboard/src/main.tsx
@@ -2,17 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'antd/dist/reset.css'
-import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider theme={{
-      token : {
-        colorPrimary : "#266AB6"
-      }
-    }}>
-      <App />
-    </ConfigProvider>
+    <App />
   </React.StrictMode>,
 )
